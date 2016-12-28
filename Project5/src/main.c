374debffc1956e60cac65f566af77b3f5e88dbcf
gulsumgudukbay <gulsum.gudukbay@ug.bilkent.edu.tr>
2016-12-28 20:41:28 +0200

    yo

---
+++ Project5/src/main.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Dogukan Yigit Polat & Gulsum Gudukbay
+ Version     :
+ Copyright   : Ours and only ours.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
